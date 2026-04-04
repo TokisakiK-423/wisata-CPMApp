45168a42d4d03b98d2de2a3a0d04606c1f91a228
TokisakiK-423 <univerword@gmail.com>
2026-04-04 14:24:16 +0700

    Update prisma.config.ts

--- api/prisma.config.ts
+++ api/prisma.config.ts
@@ -1,15 +1,9 @@
 import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
-const url = process.env.DATABASE_URL;
-
-if (!url) {
-  throw new Error('DATABASE_URL belum diisi di .env');
-}
-
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url,
+    url: process.env.DATABASE_URL!,
   },
 });

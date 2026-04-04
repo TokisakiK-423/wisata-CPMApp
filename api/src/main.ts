1d812452c55a19f934eec3d8f8278f86a4c3a694
TokisakiK-423 <univerword@gmail.com>
2026-04-04 14:15:13 +0700

    Update main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // + prefix "api"
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3001);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('CPMApp API')
+    .setDescription('API Wisata Curup Putri Malu')
+    .setVersion('1.0')
+    .addTag('wisata')
+    .addTag('galeri')
+    .addTag('review')
+    .addTag('booking')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
-void bootstrap();
+bootstrap();

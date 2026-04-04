bb57f622374032de3486b6157025ef776f3b230b
TokisakiK-423 <univerword@gmail.com>
2026-04-04 13:46:39 +0700

    Add files via upload

---
+++ api/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { KategoriModule } from './kategori/kategori.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    KategoriModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

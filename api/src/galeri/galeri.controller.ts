9a3797c92f2686a94bcf34cda9cbe82a56761c2f
TokisakiK-423 <univerword@gmail.com>
2026-04-04 14:27:20 +0700

    Update galeri.controller.ts

--- api/src/galeri/galeri.controller.ts
+++ api/src/galeri/galeri.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { GaleriService } from './galeri.service';
 import { CreateGaleriDto } from './dto/create-galeri.dto';
-import { UpdateGaleriDto } from './dto/update-galeri.dto';
 
+@ApiTags('galeri')
 @Controller('galeri')
 export class GaleriController {
   constructor(private readonly galeriService: GaleriService) {}
 
   @Post()
-  create(@Body() createGaleriDto: CreateGaleriDto) {
-    return this.galeriService.create(createGaleriDto);
+  create(@Body() dto: CreateGaleriDto) {
+    return this.galeriService.create(dto);
   }
 
   @Get()
@@ -17,18 +18,8 @@ export class GaleriController {
     return this.galeriService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.galeriService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGaleriDto: UpdateGaleriDto) {
-    return this.galeriService.update(+id, updateGaleriDto);
-  }
-
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.galeriService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.galeriService.remove(id);
   }
 }

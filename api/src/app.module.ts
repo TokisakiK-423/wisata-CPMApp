d7678d86ba693f79c0d544b697a49167d4e9e6b8
TokisakiK-423 <univerword@gmail.com>
2026-04-04 14:35:03 +0700

    Update app.module.ts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { WisataModule } from './wisata/wisata.module';
+import { GaleriModule } from './galeri/galeri.module';
+import { ReviewModule } from './review/review.module';
+import { BookingModule } from './booking/booking.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, WisataModule],
+  imports: [
+    PrismaModule,
+    WisataModule,
+    GaleriModule,
+    ReviewModule,
+    BookingModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}

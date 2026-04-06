8e2c2283e041b450d0abd5307ebefecc40acd4f9
TokisakiK-423 <univerword@gmail.com>
2026-04-06 12:09:29 +0700

    Remove ReviewService from ReviewController tests

--- api/src/review/review.controller.spec.ts
+++ api/src/review/review.controller.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReviewController } from './review.controller';
-import { ReviewService } from './review.service';
 
 describe('ReviewController', () => {
   let controller: ReviewController;
@@ -8,7 +7,6 @@ describe('ReviewController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReviewController],
-      providers: [ReviewService],
     }).compile();
 
     controller = module.get<ReviewController>(ReviewController);

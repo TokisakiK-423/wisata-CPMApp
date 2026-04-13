4d4849c2174d267aef9424257b10edf50f55b6f5
TokisakiK-423 <univerword@gmail.com>
2026-04-13 14:53:36 +0700

    Update _layout.tsx

--- mobile/app/admin/_layout.tsx
+++ mobile/app/admin/_layout.tsx
@@ -1,6 +1,11 @@
 import { Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
 
 export default function AdminLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
 }

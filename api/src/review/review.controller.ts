f5ba0c3853358b170705139f6d3ced04b2843006
TokisakiK-423 <univerword@gmail.com>
2026-04-13 14:40:58 +0700

    Update review.controller.ts

--- api/src/review/review.controller.ts
+++ api/src/review/review.controller.ts
@@ -1,36 +1,59 @@
-import { 
-  Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Req, 
-  UseGuards
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Param,
+  Body,
+  UseGuards,
+  Req,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { ReviewService } from './review.service';
-import { CreateReviewDto } from './dto/create-review.dto';
-import { JwtAuthGuard } from '../auth/jwt.guard';
-import { RolesGuard } from '../auth/roles.guard';
-import { Roles } from '../auth/roles.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
-@ApiTags('review')
+import { ReviewService } from './review.service';
+import { JwtGuard } from '../auth/jwt.guard';
+
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(private service: ReviewService) {}
 
+  @UseGuards(JwtGuard)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './public/uploads',
+        filename: (req, file, cb) => {
+          const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9);
+
+          const ext = extname(file.originalname);
+
+          cb(null, uniqueName + ext);
+        },
+      }),
+    }),
+  )
   @Post()
-  create(@Body() dto: CreateReviewDto, @Req() req?: any) {
-    return this.reviewService.create({
-      ...dto,
-      customerId: req?.user?.role === 'customer' ? req.user.userId : null,
-    });
+  create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body: any,
+    @Req() req: any,
+  ) {
+    console.log('FILE MASUK:', file);
+
+    return this.service.create(body, req.user.id, file);
   }
 
   @Get()
   findAll() {
-    return this.reviewService.findAll();
+    return this.service.findAll();
   }
 
-  @UseGuards(JwtAuthGuard, RolesGuard)  // ← IMPORT SUDAH ADA
-  @Roles('admin')
   @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.reviewService.remove(id);
+  remove(@Param('id') id: string) {
+    return this.service.delete(+id);
   }
 }

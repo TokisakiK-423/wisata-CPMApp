d65422e694622601b9cc326ac28a604e648a7aed
TokisakiK-423 <univerword@gmail.com>
2026-04-13 14:31:08 +0700

    Update jwt.guard.ts

--- api/src/auth/jwt.guard.ts
+++ api/src/auth/jwt.guard.ts
@@ -1,5 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const req = context.switchToHttp().getRequest();
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      console.log('❌ Tidak ada Authorization header');
+      return false;
+    }
+
+    if (!authHeader.startsWith('Bearer ')) {
+      console.log('❌ Format token salah:', authHeader);
+      return false;
+    }
+
+    try {
+      const token = authHeader.split(' ')[1];
+
+      const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET as string,
+      );
+
+      req.user = decoded;
+      return true;
+    } catch (err) {
+      console.log('❌ Token error:', err.message);
+      return false;
+    }
+  }
+}

cd13b8fb498fc94e3dc902c618155d45b350c607
TokisakiK-423 <univerword@gmail.com>
2026-04-15 13:22:34 +0700

    Update auth.service.ts

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import * as jwt from 'jsonwebtoken';
 
@@ -13,25 +17,40 @@ export class AuthService {
       where: { username },
     });
 
-    if (admin && admin.password === password) {
-      return {
-        token: jwt.sign({ id: admin.id, role: 'admin' }, secret),
-        role: 'admin',
-      };
-    }
-
     const customer = await this.prisma.customer.findUnique({
       where: { username },
     });
 
-    if (customer && customer.password === password) {
+    // USERNAME TIDAK ADA DI KEDUANYA
+    if (!admin && !customer) {
+      throw new UnauthorizedException(process.env.USERNAME_FAILED);
+    }
+
+    // KALAU ADMIN
+    if (admin) {
+      if (admin.password !== password) {
+        throw new UnauthorizedException(process.env.ADMIN_FAILED);
+      }
+
       return {
-        token: jwt.sign({ id: customer.id, role: 'customer' }, secret),
-        role: 'customer',
+        token: jwt.sign({ id: admin.id, role: 'admin' }, secret),
+        role: 'admin',
+        message: process.env.ADMIN_SUCCES,
       };
     }
 
-    throw new UnauthorizedException(process.env.LOGIN_FAILED);
+    // KALAU CUSTOMER
+    if (customer) {
+      if (customer.password !== password) {
+        throw new UnauthorizedException(process.env.LOGIN_FAILED);
+      }
+
+      return {
+        token: jwt.sign({ id: customer.id, role: 'customer' }, secret),
+        role: 'customer',
+        message: process.env.CUSTOMER_SUCCES,
+      };
+    }
   }
 
   // FIX REGISTER KE CUSTOMER
@@ -43,7 +62,7 @@ export class AuthService {
     });
 
     if (existing) {
-      throw new Error(process.env.USER_SUCCES);
+      throw new BadRequestException(process.env.USER_FAILED);
     }
 
     const customer = await this.prisma.customer.create({

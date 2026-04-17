94786419abb67ea1e5a8c65d5edbe28c5d8e7a69
TokisakiK-423 <univerword@gmail.com>
2026-04-17 20:24:30 +0700

    Update review.controller.ts

--- api/src/review/review.controller.ts
+++ api/src/review/review.controller.ts
@@ -1,1 +1,33 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Param,
+  Body,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { ReviewService } from './review.service';
+import { JwtGuard } from '../auth/jwt.guard';
 
+@Controller('review')
+export class ReviewController {
+  constructor(private service: ReviewService) {}
+
+  @UseGuards(JwtGuard)
+  @Post()
+  create(@Body() body, @Req() req) {
+    return this.service.create(body, req.user);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.delete(+id);
+  }
+}

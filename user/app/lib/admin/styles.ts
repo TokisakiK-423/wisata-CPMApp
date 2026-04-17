51ea2ddb08ea2543c7d26f30a1f7ad0c7fce29e5
TokisakiK-423 <univerword@gmail.com>
2026-04-17 21:30:31 +0700

    Update styles.ts

--- user/app/lib/admin/styles.ts
+++ user/app/lib/admin/styles.ts
@@ -2,6 +2,12 @@ import { StyleSheet } from 'react-native';
 import { COLORS } from './colors';
 
 export const styles = StyleSheet.create({
+  background: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+
   tabBar: {
     backgroundColor: COLORS.white,
     borderTopWidth: 0,

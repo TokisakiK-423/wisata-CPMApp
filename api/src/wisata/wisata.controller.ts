475379faf5c36a9199fc153fa2cdd958c70c7f1f
TokisakiK-423 <univerword@gmail.com>
2026-04-17 20:25:23 +0700

    Update wisata.controller.ts

--- api/src/wisata/wisata.controller.ts
+++ api/src/wisata/wisata.controller.ts
@@ -1,1 +1,22 @@
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
+import { WisataService } from './wisata.service';
 
+@Controller('wisata')
+export class WisataController {
+  constructor(private service: WisataService) {}
+
+  @Post()
+  create(@Body() body) {
+    return this.service.create(body);
+  }
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.delete(+id);
+  }
+}

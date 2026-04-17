4ebd5bb2f616743d5a6c19968a56ab984513671f
TokisakiK-423 <univerword@gmail.com>
2026-04-17 20:21:09 +0700

    Update jwt.guard.ts

--- api/src/auth/jwt.guard.ts
+++ api/src/auth/jwt.guard.ts
@@ -1,1 +1,22 @@
 
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const req = context.switchToHttp().getRequest();
+    const auth = req.headers.authorization;
+
+    if (!auth) return false;
+
+    try {
+      const token = auth.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      return true;
+    } catch {
+      return false;
+    }
+  }
+}

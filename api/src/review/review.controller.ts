8a76804cafe8182c00f33f84a14d457c40c19c91
TokisakiK-423 <univerword@gmail.com>
2026-04-17 21:20:22 +0700

    Update review.controller.ts

--- api/src/review/review.controller.ts
+++ api/src/review/review.controller.ts
@@ -2,10 +2,15 @@ import {
   Controller,
   Post,
   Get,
+  Delete,
+  Param,
   Body,
   UseGuards,
   Req,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ReviewService } from './review.service';
 import { JwtGuard } from '../auth/jwt.guard';
 
@@ -14,13 +19,23 @@ export class ReviewController {
   constructor(private service: ReviewService) {}
 
   @UseGuards(JwtGuard)
+  @UseInterceptors(FileInterceptor('image')) // 🔥 DI SINI
   @Post()
-  create(@Body() body: any, @Req() req: any) {
-    return this.service.create(body, req.user.id);
+  create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body: any,
+    @Req() req: any,
+  ) {
+    return this.service.create(body, req.user.id, file);
   }
 
   @Get()
   findAll() {
     return this.service.findAll();
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.delete(+id);
   }
 }

e7da4b73d395d0966c3f58c390c665306bea354d
TokisakiK-423 <univerword@gmail.com>
2026-04-17 21:28:12 +0700

    Update styles.ts

--- user/app/lib/admin/styles.ts
+++ user/app/lib/admin/styles.ts
@@ -1,15 +1,9 @@
 import { StyleSheet } from 'react-native';
-
-export const COLORS = {
-  primary: '#7c3aed', // ungu
-  secondary: '#ec4899', // pink
-  white: '#ffffff',
-  text: '#1f2937',
-};
+import { COLORS } from './colors';
 
 export const styles = StyleSheet.create({
   tabBar: {
-    backgroundColor: '#fff',
+    backgroundColor: COLORS.white,
     borderTopWidth: 0,
     height: 65,
     paddingBottom: 8,

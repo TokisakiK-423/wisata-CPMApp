e95f2d47744e9071695a0072beab3b6ebe4945ee
TokisakiK-423 <univerword@gmail.com>
2026-04-17 20:22:55 +0700

    Update galeri.controller.ts

--- api/src/galeri/galeri.controller.ts
+++ api/src/galeri/galeri.controller.ts
@@ -1,1 +1,23 @@
 
+import { Controller, Post, Get, Delete, Param, Body } from '@nestjs/common';
+import { GaleriService } from './galeri.service';
+
+@Controller('galeri')
+export class GaleriController {
+  constructor(private service: GaleriService) {}
+
+  @Post()
+  create(@Body() body) {
+    return this.service.create(body);
+  }
+
+  @Get(':wisataId')
+  find(@Param('wisataId') id: string) {
+    return this.service.findByWisata(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.delete(+id);
+  }
+}

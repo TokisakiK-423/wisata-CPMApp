7bd2a38c362598429e6d0d0ca5d1403ab8068c39
TokisakiK-423 <univerword@gmail.com>
2026-04-17 21:23:13 +0700

    Update main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // 🔥 WAJIB biar folder public bisa diakses
-  app.use('/uploads', express.static(join(process.cwd(), 'public/uploads')));
+  // 🔥 cukup ini saja
+  app.useStaticAssets(join(process.cwd(), 'public'), {
+    prefix: '/',
+  });
 
   await app.listen(3000);
 }

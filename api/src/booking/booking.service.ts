e5b2a0618877fcdc7356c55ef8e5104c1a87ac3f
TokisakiK-423 <univerword@gmail.com>
2026-04-17 20:21:50 +0700

    Update booking.service.ts

--- api/src/booking/booking.service.ts
+++ api/src/booking/booking.service.ts
@@ -1,1 +1,51 @@
 
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class BookingService {
+  constructor(private prisma: PrismaService) {}
+
+  create(dto, user) {
+    return this.prisma.booking.create({
+      data: {
+        wisataId: dto.wisataId,
+        nama: dto.nama,
+        noHp: dto.noHp,
+        jumlahTiket: dto.jumlahTiket,
+        customerId: user.id,
+      },
+    });
+  }
+
+  findAll() {
+    return this.prisma.booking.findMany({
+      include: {
+        wisata: true,
+        customer: true,
+      },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+
+  findMy(user) {
+    return this.prisma.booking.findMany({
+      where: { customerId: user.id },
+      include: { wisata: true },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+
+  updateStatus(id: number, status: string) {
+    return this.prisma.booking.update({
+      where: { id },
+      data: { status },
+    });
+  }
+
+  delete(id: number) {
+    return this.prisma.booking.delete({
+      where: { id },
+    });
+  }
+}

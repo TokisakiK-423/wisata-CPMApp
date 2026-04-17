e6467b640a4758de7ef44e105ea38ad5b0f2a664
TokisakiK-423 <univerword@gmail.com>
2026-04-17 19:39:39 +0700

    Delete user/app/customer directory

--- user/app/customer/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-export default function CustomerLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
-}

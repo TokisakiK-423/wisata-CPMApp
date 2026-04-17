ebee759f93266c90d03a470414ac045ed64683d0
TokisakiK-423 <univerword@gmail.com>
2026-04-17 19:39:49 +0700

    Delete user/app/lib directory

--- user/app/lib/admin/utils/auth.ts
+++
@@ -1,7 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { router } from 'expo-router';
-
-export const logout = async () => {
-  await AsyncStorage.multiRemove(['token', 'role', 'user']);
-  router.replace('/login');
-};
--- user/app/lib/customer/colors.ts
+++
@@ -1,6 +0,0 @@
-export const COLORS = {
-  primary: "#7b2ff7",
-  secondary: "#f107a3",
-  white: "#ffffff",
-  gray: "#9ca3af",
-};

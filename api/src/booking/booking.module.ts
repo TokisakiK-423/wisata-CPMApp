0137c5715da8305527d4ec50c3ffecb07d10caef
TokisakiK-423 <univerword@gmail.com>
2026-04-17 23:52:06 +0700

    Update booking.module.ts

--- api/src/booking/booking.module.ts
+++ api/src/booking/booking.module.ts
@@ -1,18 +1,10 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
+import { BookingController } from './booking.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
-describe('BookingService', () => {
-  let service: BookingService;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [BookingService],
-    }).compile();
-
-    service = module.get<BookingService>(BookingService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+@Module({
+  controllers: [BookingController],
+  providers: [BookingService, PrismaService],
+})
+export class BookingModule {}

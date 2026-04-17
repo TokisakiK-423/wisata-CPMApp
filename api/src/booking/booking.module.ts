fcbb4e1e4c07509742167687efe96ee2758ab277
TokisakiK-423 <univerword@gmail.com>
2026-04-17 21:00:54 +0700

    Update booking.module.ts

--- api/src/booking/booking.module.ts
+++ api/src/booking/booking.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { WisataService } from '../wisata/wisata.service';
 
 @Module({
   controllers: [BookingController],
-  providers: [BookingService, PrismaService],
+  providers: [BookingService, PrismaService, WisataService],
 })
 export class BookingModule {}

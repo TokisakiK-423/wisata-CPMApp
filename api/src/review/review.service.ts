2fb173e8a58f706923fcd3f15e64c7147e90c186
TokisakiK-423 <univerword@gmail.com>
2026-04-17 20:24:16 +0700

    Update review.service.ts

--- api/src/review/review.service.ts
+++ api/src/review/review.service.ts
@@ -1,1 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
+@Injectable()
+export class ReviewService {
+  constructor(private prisma: PrismaService) {}
+
+  create(dto, user) {
+    return this.prisma.review.create({
+      data: {
+        wisataId: dto.wisataId,
+        rating: dto.rating,
+        komentar: dto.komentar,
+        nama: dto.nama,
+        customerId: user.id,
+      },
+    });
+  }
+
+  findAll() {
+    return this.prisma.review.findMany({
+      include: {
+        wisata: true,
+        customer: true,
+      },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+
+  delete(id: number) {
+    return this.prisma.review.delete({
+      where: { id },
+    });
+  }
+}

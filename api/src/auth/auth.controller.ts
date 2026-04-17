09b43ca710f869ff5bb2a9027fbd068ff3797aa6
TokisakiK-423 <univerword@gmail.com>
2026-04-17 20:20:57 +0700

    Update auth.controller.ts

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,1 +1,12 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
+@Controller('auth')
+export class AuthController {
+  constructor(private service: AuthService) {}
+
+  @Post('login')
+  login(@Body() body) {
+    return this.service.login(body.username, body.password);
+  }
+}

3fbff28c57b332a4c8b4c3d4627858629dc6f74b
TokisakiK-423 <univerword@gmail.com>
2026-04-17 20:25:10 +0700

    Update wisata.service.ts

--- api/src/wisata/wisata.service.ts
+++ api/src/wisata/wisata.service.ts
@@ -1,1 +1,28 @@
 
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class WisataService {
+  constructor(private prisma: PrismaService) {}
+
+  create(dto) {
+    return this.prisma.wisata.create({ data: dto });
+  }
+
+  findAll() {
+    return this.prisma.wisata.findMany({
+      include: {
+        galeri: true,
+        reviews: true,
+      },
+      orderBy: { id: 'desc' },
+    });
+  }
+
+  delete(id: number) {
+    return this.prisma.wisata.delete({
+      where: { id },
+    });
+  }
+}

670085f7695fd2080e46606974eb9b696df21856
TokisakiK-423 <univerword@gmail.com>
2026-04-17 20:26:56 +0700

    Update main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  dotenv.config();
 
-  app.useStaticAssets(join(process.cwd(), 'public'), {
-    prefix: '/',
-  });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   await app.listen(3000);
 }

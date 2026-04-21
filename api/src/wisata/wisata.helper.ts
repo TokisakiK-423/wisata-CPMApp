e459e266905a72f587de1fba4b2f459fc2009eef
TokisakiK-423 <univerword@gmail.com>
2026-04-21 14:02:20 +0700

    Update wisata.helper.ts

--- api/src/wisata/wisata.helper.ts
+++ api/src/wisata/wisata.helper.ts
@@ -1,1 +1,33 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { deleteGaleriFiles } from '../common/utils/file.utils';
 
+@Injectable()
+export class WisataHelper {
+  constructor(private prisma: PrismaService) {}
+
+  async findWisataOrFail(id: number) {
+    const wisata = await this.prisma.wisata.findUnique({
+      where: { id },
+      include: { galeri: true },
+    });
+
+    if (!wisata) {
+      throw new BadRequestException(process.env.WISATA_FAILED);
+    }
+
+    return wisata;
+  }
+
+  async deleteGaleri(id: number) {
+    const galeri = await this.prisma.galeri.findMany({
+      where: { wisataId: id },
+    });
+
+    deleteGaleriFiles(galeri);
+
+    await this.prisma.galeri.deleteMany({
+      where: { wisataId: id },
+    });
+  }
+}

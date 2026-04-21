dbb24f7b177e432a8fc3aecf5b8448682ddb3953
TokisakiK-423 <univerword@gmail.com>
2026-04-21 13:55:16 +0700

    Update auth.module.ts

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthHelper } from './auth.helper';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, PrismaService, AuthHelper],
 })
 export class AuthModule {}

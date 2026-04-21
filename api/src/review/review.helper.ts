b0e619114023be9348849970302c974831a24fa2
TokisakiK-423 <univerword@gmail.com>
2026-04-21 14:00:04 +0700

    Update review.helper.ts

--- api/src/review/review.helper.ts
+++ api/src/review/review.helper.ts
@@ -1,1 +1,38 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
+@Injectable()
+export class ReviewHelper {
+  constructor(private prisma: PrismaService) {}
+
+  async findCustomerOrFail(customerId: number) {
+    const customer = await this.prisma.customer.findUnique({
+      where: { id: customerId },
+    });
+
+    if (!customer) {
+      throw new BadRequestException(process.env.CUSTOMER_FAILED);
+    }
+
+    return customer;
+  }
+
+  async findReviewOrFail(id: number) {
+    const review = await this.prisma.review.findUnique({
+      where: { id },
+    });
+
+    if (!review) {
+      throw new BadRequestException(process.env.RIVIEW_FAILED);
+    }
+
+    return review;
+  }
+
+  validateOwner(reviewCustomerId: number | null, userId: number) {
+    // kalau null → otomatis tidak punya pemilik
+    if (!reviewCustomerId || reviewCustomerId !== userId) {
+      throw new BadRequestException(process.env.USERS_DO_NOT);
+    }
+  }
+}

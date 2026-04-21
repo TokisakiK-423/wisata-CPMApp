00d3df3dd5a1816608d46182729fd8c9de77bcae
TokisakiK-423 <univerword@gmail.com>
2026-04-21 14:02:54 +0700

    Update wisata.module.ts

--- api/src/wisata/wisata.module.ts
+++ api/src/wisata/wisata.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { WisataService } from './wisata.service';
 import { WisataController } from './wisata.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { WisataHelper } from './wisata.helper';
 
 @Module({
   controllers: [WisataController],
-  providers: [WisataService, PrismaService],
+  providers: [WisataService, PrismaService, WisataHelper],
   exports: [WisataService],
 })
 export class WisataModule {}

33fbc67c9334775cab802695119ad9f738d0bc28
TokisakiK-423 <univerword@gmail.com>
2026-04-21 13:57:37 +0700

    Update booking.helper.ts

--- api/src/booking/booking.helper.ts
+++ api/src/booking/booking.helper.ts
@@ -1,1 +1,31 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
+@Injectable()
+export class BookingHelper {
+  constructor(private prisma: PrismaService) {}
+
+  async findWisataOrFail(id: number) {
+    const wisata = await this.prisma.wisata.findUnique({
+      where: { id },
+    });
+
+    if (!wisata) {
+      throw new BadRequestException(process.env.IDWISATA_FAILED);
+    }
+
+    return wisata;
+  }
+
+  async findBookingOrFail(id: number) {
+    const booking = await this.prisma.booking.findUnique({
+      where: { id },
+    });
+
+    if (!booking) {
+      throw new BadRequestException(process.env.UPBOOKING_FAILED);
+    }
+
+    return booking;
+  }
+}

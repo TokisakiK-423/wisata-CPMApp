4b90ffd73cee7b9979b5de4b8255e6b90fbfce11
TokisakiK-423 <univerword@gmail.com>
2026-04-21 13:55:34 +0700

    Update auth.service.ts

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,69 +1,36 @@
-import {
-  BadRequestException,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import * as jwt from 'jsonwebtoken';
+import { AuthHelper } from './auth.helper';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private helper: AuthHelper,
+  ) {}
 
   async login(username: string, password: string) {
     const secret = process.env.JWT_SECRET as string;
 
-    const admin = await this.prisma.admin.findUnique({
-      where: { username },
-    });
+    const user = await this.helper.findUser(username);
 
-    const customer = await this.prisma.customer.findUnique({
-      where: { username },
-    });
+    this.helper.validatePassword(user.password, password, user.role);
 
-    // USERNAME TIDAK ADA DI KEDUANYA
-    if (!admin && !customer) {
-      throw new UnauthorizedException(process.env.USERNAME_FAILED);
-    }
-
-    // KALAU ADMIN
-    if (admin) {
-      if (admin.password !== password) {
-        throw new UnauthorizedException(process.env.ADMIN_FAILED);
-      }
-
-      return {
-        token: jwt.sign({ id: admin.id, role: 'admin' }, secret),
-        role: 'admin',
-        message: process.env.ADMIN_SUCCES,
-      };
-    }
-
-    // KALAU CUSTOMER
-    if (customer) {
-      if (customer.password !== password) {
-        throw new UnauthorizedException(process.env.LOGIN_FAILED);
-      }
-
-      return {
-        token: jwt.sign({ id: customer.id, role: 'customer' }, secret),
-        role: 'customer',
-        message: process.env.CUSTOMER_SUCCES,
-      };
-    }
+    return {
+      token: jwt.sign({ id: user.id, role: user.role }, secret),
+      role: user.role,
+      message:
+        user.role === 'admin'
+          ? process.env.ADMIN_SUCCES
+          : process.env.CUSTOMER_SUCCES,
+    };
   }
 
-  // FIX REGISTER KE CUSTOMER
   async register(data: any) {
     const { username, password, nama } = data;
 
-    const existing = await this.prisma.customer.findUnique({
-      where: { username },
-    });
-
-    if (existing) {
-      throw new BadRequestException(process.env.USER_FAILED);
-    }
+    await this.helper.checkCustomerExists(username);
 
     const customer = await this.prisma.customer.create({
       data: {

0b8d0451a435f25a70577e3e226b14064e202edb
TokisakiK-423 <univerword@gmail.com>
2026-04-21 14:01:02 +0700

    Update review.module.ts

--- api/src/review/review.module.ts
+++ api/src/review/review.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { ReviewHelper } from './review.helper';
 
 @Module({
   controllers: [ReviewController],
-  providers: [ReviewService, PrismaService],
+  providers: [ReviewService, PrismaService, ReviewHelper],
 })
 export class ReviewModule {}

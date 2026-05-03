2d18551e75f89778eccfb384816dee406394623b
divkaferdiansyah <divkaferdiansyah6220@gmail.com>
2026-05-03 23:41:48 +0700

    Add files via upload

---
+++ cms/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { setToken } from "./lib/api";
+
+const token = localStorage.getItem("token");
+if (token) setToken(token);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

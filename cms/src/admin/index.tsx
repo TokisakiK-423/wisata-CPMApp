a729878d40e8b85c4b6a0be03286a25759b685a4
divkaferdiansyah <divkaferdiansyah6220@gmail.com>
2026-05-08 21:24:01 +0700

    Update index.tsx

--- cms/src/admin/index.tsx
+++ cms/src/admin/index.tsx
@@ -97,6 +97,22 @@ export default function AdminHome() {
       boxShadow: "0 4px 10px rgba(37,99,235,0.3)",
     }}
   >
+    Data Booking
+  </button>
+
+  <button
+    onClick={() => navigate("/admin/review")}
+    style={{
+      background: "#2563eb",
+      color: "white",
+      border: "none",
+      padding: "10px 18px",
+      borderRadius: 8,
+      cursor: "pointer",
+      fontWeight: "bold",
+      boxShadow: "0 4px 10px rgba(37,99,235,0.3)",
+    }}
+  >
       <h2>Dashboard Admin</h2>
 
       <button onClick={() => navigate("/admin/wisata")}>

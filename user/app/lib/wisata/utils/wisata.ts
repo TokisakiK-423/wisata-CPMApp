2e1cd37466fa6b968fd514af3d5db7b243c8a458
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-06-13 21:31:59 +0700

    Update koding wisata.ts. pada File : wisata-CPMApp\user\app\lib\wisata\utils\wisata.ts

--- user/app/lib/wisata/utils/wisata.ts
+++ user/app/lib/wisata/utils/wisata.ts
@@ -1,6 +1,8 @@
+import { apiUrl } from '@/app/lib/api';
+
 export const fetchWisataDetail = async (id: string | number) => {
   try {
-    const res = await fetch(`http://10.0.2.2:3000/wisata/${id}`);
+    const res = await fetch(apiUrl(`/wisata/${id}`));
     if (!res.ok) throw new Error("Gagal ambil data");
 
     const data = await res.json();
